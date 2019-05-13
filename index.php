5c426e9afa014e46b7b8a226d71cde019fdc6d52
sbusile <50584834+sbusile@users.noreply.github.com>
2019-05-13 16:11:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php
-
-?>
-
+<html>
+  <head></head>
+  <body>
 <h1>Front Page</h1>
 <a href="login.php">Login To Account</a>
 <br><br>
 <a href="register.php">Create An Account</a>
+    </body>
+</html>
